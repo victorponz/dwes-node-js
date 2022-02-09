86222dd9ebe799eb07cc2f1c00470efded8675a8
Víctor Ponz <victorponz@gmail.com>
2022-02-09 16:43:35 +0100

    Versión final

--- server.js
+++ server.js
@@ -3,13 +3,25 @@ const Settings = require('./settings');
 const server = new Hapi.Server({ port: Settings.port });
 const Routes = require('./lib/routes');
 const Models = require('./lib/models');
-
-server.route(Routes);
-
+const Path = require('path');
+const Pug = require('pug');
+const Vision = require('vision');
+const Inert = require('inert');
 const init = async () => {
+    await server.register([ Vision, Inert ]);
+    server.views({
+        engines: { pug: Pug },
+        path: Path.join(__dirname, 'lib/views'),
+        compileOptions: {
+            pretty: false,
+        },
+        isCached: Settings.env === 'production'
+    });
+    server.route(Routes);
     await server.start();
     console.log(`Server running at: ${server.info.uri}`);
 };
+
 process.on('unhandledRejection', (err) => {
     console.log(err);
     process.exit(1);
---
+++ static/public/scripts/main.js
@@ -0,0 +1,26 @@
+var $noteForm = $('#note-form');
+
+$noteForm.submit(function (e) {
+  e.preventDefault();
+
+  var form = {
+    url: $(this).attr('action'),
+    type: $(this).attr('method')
+  };
+
+  $.ajax({
+    url: form.url,
+    type: form.type,
+    data: $(this).serialize(),
+    success: function (result) {
+      $.modal.close();
+
+      if (form.type === 'POST') {
+        $('.notes-list').prepend(result);
+        $noteForm.find('input[type=text], textarea').val('');
+      } else if (form.type === 'PUT') {
+        $('.note-content').html(result);
+      }
+    }
+  });
+});
